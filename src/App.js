e61aa8d37a5a88caad8b5e49e590bc5e846a4bdc
Neetish Bamotraa <66291255+NeetishBamotraa@users.noreply.github.com>
2022-04-30 14:20:30 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./style.css";
+
+import bgimage from "./assets/bg.png";
+import navlogo from "./assets/logo.png";
+import SearchIcon from "@mui/icons-material/Search";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="main">
+      <div className="mainbgimage">
+        <img src={bgimage} />
+        <div className="overlay"></div>
+      </div>
+      <header className="navbar">
+        <div className="navbarlogo">
+          <img src={navlogo} />
+        </div>
+        <div className="navbaroptions">
+          <div className="search">
+            <div className="icon">
+              <SearchIcon />
+            </div>
+            <div className="title">Search</div>
+          </div>
+          <div className="help">Help</div>
+          <div className="account">Account</div>
+          <div className="iconcart">b</div>
+        </div>
       </header>
     </div>
   );
